4821988c405ef51dadc6ccb374193c83cc9c92a3
MyusufUAEU <111703181+MyusufUAEU@users.noreply.github.com>
2022-08-21 22:16:25 +0400

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,24 @@
+
+library(shiny)
+
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Changing the bins of a histogram for Stat210"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
